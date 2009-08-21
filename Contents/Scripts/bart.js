92ff3ca83025f78296903ec266fa59f29f2e482a
Eugene Koontz <ekoontz@Macintosh-2.local>
2009-08-20 23:29:02 -0700

    showing two time estimates (powell to dublin/pleasanton; macarthur to richmond

--- Contents/Scripts/bart.js
+++ Contents/Scripts/bart.js
@@ -1,5 +1,8 @@
 log("reading 'bart_eta.xml'");
-var vitalityDoc = XMLDOM.parse( filesystem.readFile( "bart_eta.xml" ) );
+var bartEtaDoc = XMLDOM.parse( filesystem.readFile( "bart_eta.xml" ) );
+
+stations = bartEtaDoc.evaluate( "root/station/name" );
+
 log("finished reading 'bart_eta.xml'");
 
 log("opening bart database.");
@@ -7,33 +10,67 @@ bartDB = new SQLite( );
 bartDB.open( system.widgetDataFolder + "/bart.db" );
 log("finished reading bart database.");
 
-table_data_frame = widget.getElementById("barttable");
+table_data_frame = bartWindow.getElementById("barttable");
 
-bart_row = new bartRow("Ashby station");
-bart_row.vOffset = 30;
-table_data_frame.appendChild(bart_row);
+var vOffset = 0;
+var station = "Powell St.";
 
-bart_row = new bartRow("Powell station");
-bart_row.vOffset = 45;
-table_data_frame.appendChild(bart_row);
+function myStatusProc() {
+    log("got here..(myStatusProc()");
+}
 
-bart_row = new bartRow("16th St station");
-bart_row.vOffset = 60;
-table_data_frame.appendChild(bart_row);
+    var request = new XMLHttpRequest();
+    request.onreadystateexchange = myStatusProc;
+    request.open("GET","http://www.bart.gov/dev/eta/bart_eta.xml",false);
+    request.send();
+    if (request.status == 200) {
+	arrival_times = XMLDOM.parse(request.responseXML.toXML());
+    }
+    else {
+	log("could not retrieve response from http://www.bart.gov.");
+    }
 
-bart_row = new bartRow("MacArthur station");
-bart_row.vOffset = 75;
-table_data_frame.appendChild(bart_row);
+    // 1st leg of journey
+    bart_row = new bartStation("Powell to Dublin/Pleasanton");
+    table_data_frame.appendChild(bart_row);
+    estimate = arrival_times.evaluate("string(/root/station[name='Powell St.']/eta[destination='Dublin/Pleasanton']/estimate)");
+    bart_row = new bartStation(estimate);
+    table_data_frame.appendChild(bart_row);
 
+    // 2nd leg of journey
+    bart_row = new bartStation("MacArthur to Richmond");
+    table_data_frame.appendChild(bart_row);
+
+    estimate = arrival_times.evaluate("string(/root/station[name='MacArthur']/eta[destination='Richmond']/estimate)");
+    bart_row = new bartStation(estimate);
+    table_data_frame.appendChild(bart_row);
+
+//refreshPrefs.onTimerFired = loadStations();
+
+function loadStations() {
+/*    station_dropdown = preferences.leaving_from;
+    log("loadStations()" + station_dropdown);
+    foo = XMLDOM.parse( filesystem.readFile( "bart_eta.xml" ) );
+    bar = foo.getElementById("foo");
+    baz = foo.createElement("option");
+    log(baz);
+    log(preferences.leaving_from);
+    test = widget.importNode(baz);
+//    preferences.leaving_from.importNode(baz);
+    refreshPrefs.interval = 10;
+*/
+}
 
 function initialize() {
 }
 
 function about() {
+    loadStations();
     alert("By Eugene Koontz (ekoontz@hiro-tan.org)");
+
 }
 
-function bartRow( text ) {
+function bartStation( text ) {
     var obj = new Frame( );
     
     obj.symbol = null;
@@ -44,15 +81,16 @@ function bartRow( text ) {
     obj.width = 200;
     obj.height = 28;
     obj.hOffset = 30;
-    
-    obj.stockSymbol = new Text( );
-    obj.stockSymbol.style.fontFamily = "'Arial Black'";
-    obj.stockSymbol.style.fontSize = "14px";
-    obj.stockSymbol.style.color = "#00ff00";
-    obj.stockSymbol.hOffset = 14;
-    obj.stockSymbol.vOffset = 16;
-    obj.stockSymbol.data = text;
-    obj.appendChild( obj.stockSymbol );
+    obj.vOffset = vOffset;
+    vOffset += 15; // increment global
+    obj.text = new Text( );
+    obj.text.style.fontFamily = "'Arial Black'";
+    obj.text.style.fontSize = "14px";
+    obj.text.style.color = "#00ff00";
+    obj.text.hOffset = 14;
+    obj.text.vOffset = 16;
+    obj.text.data = text;
+    obj.appendChild( obj.text );
 
     return obj;
 }
