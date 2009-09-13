bdd9e3775897e5f769ff4ff6b9a4565de6010b73
Eugene Koontz <ekoontz@Macintosh-2.local>
2009-09-12 18:35:26 -0700

    for speed,using station abbreviations for join conditions

--- Contents/Scripts/bart.js
+++ Contents/Scripts/bart.js
@@ -332,11 +332,6 @@ function reload_etas() {
     var from_station = preferences.start_station.value;
     var to_station = preferences.end_station.value;
 
-    var transfer_station = "12th St. Oakland City Center";
-    var final_destination_first_leg = "Pittsburg/Bay Point";
-    var final_destination_second_leg = "Richmond";
-    var final_destination;
-
     // remove old messages from barttable.
     content_to_remove = table_data_frame.evaluate("frame");
     for (var i = 0; i < content_to_remove.length; i++) {
@@ -345,51 +340,72 @@ function reload_etas() {
     table_data_frame.home();
     vOffset = 0;
 
+    var station_name_query = ""+
+	"SELECT A.abbr AS A_abbr,B.abbr AS B_abbr " +
+         " FROM station A " +
+    "INNER JOIN station B ON (A.name='"+from_station+"') AND (B.name='"+to_station+"')";
+
+    var station_name_result = db.query(station_name_query);
+    var station_name_row = station_name_result.getRow();
+    var station_a_abbr = station_name_row['A_abbr'];
+    var station_b_abbr = station_name_row['B_abbr'];
+
     var find_q = ""+
-"SELECT A.from_station AS from_station,A.final_destination AS bound_to1,A_line_from.color,"+
-"       B.from_station AS transfer_at,D.final_destination AS bound_to2,D.from_station AS final_destination,"+
-"       C_line_from.color,(A.distance - B.distance) + (C.distance - D.distance)      "+
+"SELECT A_station.name AS from_station,A_bound_to.name AS bound_to1,A_line_from.color,"+
+"       B_station.name AS transfer_at,"+
+"       D_bound_to.name AS bound_to2,D_station.name AS final_destination,"+
+"       C_line_from.color,"+
+"       (A.distance - B.distance) + (C.distance - D.distance)      "+
 "      FROM d_before A    "+
-"INNER JOIN d_before B   "+
+" INNER JOIN d_before B   "+
 "        ON (A.final_destination = B.final_destination)  "+
 "       AND (A.from_station <> B.from_station)"+
-"INNER JOIN d_before C            "+
+" INNER JOIN station A_station"+
+"        ON A_station.abbr = A.from_station"+
+" INNER JOIN station A_bound_to"+
+"        ON A_bound_to.abbr = A.final_destination"+
+" INNER JOIN d_before C            "+
 "        ON (B.from_station = C.from_station) "+
-"INNER JOIN d_before D     "+
+" INNER JOIN d_before D     "+
 "        ON (C.final_destination = D.final_destination)     "+
 "       AND (C.from_station <> D.from_station)"+
-"INNER JOIN line A_line_from       "+
+" INNER JOIN station B_station"+
+"        ON B_station.abbr = B.from_station"+
+" INNER JOIN line A_line_from       "+
 "        ON (A_line_from.station = A.from_station)"+
-"INNER JOIN line A_line_destination       "+
+" INNER JOIN line A_line_destination       "+
 "        ON (A_line_destination.station = A.final_destination)"+
 "       AND (A_line_from.color = A_line_destination.color)"+
-"INNER JOIN line B_line_from"+
+" INNER JOIN line B_line_from"+
 "        ON (B_line_from.station = B.from_station)   "+
 "       AND (A_line_from.color = B_line_from.color)"+
-"INNER JOIN line B_line_destination       "+
+" INNER JOIN line B_line_destination       "+
 "        ON (B_line_destination.station = B.final_destination)"+
 "       AND (B_line_from.color = B_line_destination.color)"+
-"INNER JOIN line C_line_from           "+
-"        ON (C_line_from.station = C.from_station)      "+
-"INNER JOIN line C_line_destination                 "+
-"        ON (C_line_destination.station = C.final_destination)      "+
-"       AND (C_line_from.color = C_line_destination.color)      "+
-"INNER JOIN line D_line              "+
-"        ON (D_line.station = D.from_station)        "+
-"       AND (C_line_from.color = D_line.color)         "+
-"     WHERE A.from_station = 'POWL'          "+
-"       AND D.from_station = 'ASHB'        "+
-"       AND (A.distance > B.distance)       "+
-"       AND (C.distance > D.distance)       "+
-"  ORDER BY (A.distance - B.distance) + (C.distance - D.distance)";
+" INNER JOIN line C_line_from           "+
+"        ON (C_line_from.station = C.from_station)"+
+" INNER JOIN line C_line_destination           "+
+"        ON (C_line_destination.station = C.final_destination)"+
+"       AND (C_line_from.color = C_line_destination.color)"+
+" INNER JOIN line D_line        "+
+"        ON (D_line.station = D.from_station)  "+
+"       AND (C_line_from.color = D_line.color)   "+
+" INNER JOIN station D_station"+
+"        ON D_station.abbr = D.from_station"+
+" INNER JOIN station D_bound_to"+
+"        ON D_bound_to.abbr = D.final_destination"+
+"     WHERE A.from_station = '"+station_a_abbr+"'"+
+"       AND D.from_station = '"+station_b_abbr+"'"+
+"       AND (A.distance > B.distance)"+ 
+"       AND (C.distance > D.distance) "+
+"  ORDER BY (A.distance - B.distance) + (C.distance - D.distance)"+
+" LIMIT 10;";
 
-    log(find_q);
     var find_result = db.query(find_q);
     var top_row = find_result.getRow();
     var top_from_station = top_row['from_station'];
     var top_bound_to1 = top_row['bound_to1'];
     var top_transfer_at = top_row['transfer_at'];
-    log("TRANSFER AT " + top_transfer_at);
     var top_bound_to2 = top_row['bound_to2'];
     var top_final_destination = top_row['final_destination'];
 
