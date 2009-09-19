1e02250c35a333aa0b3e85a98cffcdbbd01fc916
Eugene Koontz <ekoontz@Macintosh-2.local>
2009-09-19 12:30:45 -0700

    fix test case

--- test/oneline.sql
+++ test/oneline.sql
@@ -1,11 +1,12 @@
-      SELECT A_station.name AS from_station,
-             A_bound_to.name AS bound_to1,
-             A_line_from.color,       
-             B_station.name AS transfer_at,  
-             D_bound_to.name AS bound_to2,
-             D_station.name AS final_destination,  
-             C_line_from.color,      
-             (A.distance - B.distance) + (C.distance - D.distance)         
+  SELECT A_station.name AS from_station,
+         A_bound_to.name AS bound_to1,
+         A_line_from.color AS AB_color,       
+         B_station.name AS transfer_at,  
+         NULL AS bound_to2,
+         B.from_station AS final_destination,
+         A_line_from.color AS CD_color,
+         (A.distance - B.distance) AS AB_distance,
+	 0 AS CD_distance
         FROM d_before A   
   INNER JOIN d_before B    
           ON (A.final_destination = B.final_destination)   
@@ -14,11 +15,6 @@
           ON A_station.abbr = A.from_station 
   INNER JOIN station A_bound_to  
           ON A_bound_to.abbr = A.final_destination
-  INNER JOIN d_before C                 
-          ON (B.from_station = C.from_station) 
-  INNER JOIN d_before D         
-          ON (C.final_destination = D.final_destination)   
-         AND (C.from_station <> D.from_station) 
   INNER JOIN station B_station   
           ON B_station.abbr = B.from_station 
   INNER JOIN line A_line_from          
@@ -32,24 +28,8 @@
   INNER JOIN line B_line_destination           
           ON (B_line_destination.station = B.final_destination)   
          AND (B_line_from.color = B_line_destination.color) 
-  INNER JOIN line C_line_from            
-          ON (C_line_from.station = C.from_station)
-  INNER JOIN line C_line_destination          
-          ON (C_line_destination.station = C.final_destination) 
-         AND (C_line_from.color = C_line_destination.color)
-  INNER JOIN line D_line                
-          ON (D_line.station = D.from_station)  
-         AND (A_line_from.color = C_line_from.color)    
-         AND (C_line_from.color = D_line.color)    
-  INNER JOIN station D_station        
-          ON D_station.abbr = D.from_station 
-  INNER JOIN station D_bound_to        
-          ON D_bound_to.abbr = D.final_destination     
-       WHERE A.from_station = 'POWL'       
-         AND D.from_station = 'ASHB'       
-         AND (A.distance > B.distance)     
-         AND (C.distance > D.distance)   
-    ORDER BY A_line_from.color = C_line_from.color DESC,
-            (A.distance - B.distance) + (C.distance - D.distance);
-
-
+       WHERE 
+       A.from_station = 'ROCK' AND      
+--         AND B.from_station = 'ORIN'       
+ (A_line_from.color = B_line_from.color)
+         AND (A.distance > B.distance) ;
