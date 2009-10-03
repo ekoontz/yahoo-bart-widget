b660b957e76b494e95582134ebe100968bf06fba
Eugene Koontz <ekoontz@Macintosh-2.local>
2009-10-03 09:49:58 -0700

    compiled files from running 'Makefile'

--- Contents/tools/schema.sql
+++ Contents/tools/schema.sql
@@ -1,5 +1,14 @@
+DROP TABLE IF EXISTS adjacent;
 CREATE TABLE adjacent (station_a TEXT, station_b TEXT);
+
+DROP TABLE IF EXISTS d_before;
 CREATE TABLE d_before (from_station TEXT,final_destination TEXT, distance INTEGER);
+
+DROP TABLE IF EXISTS destination;
 CREATE TABLE destination (station TEXT,destination TEXT, eta TEXT);
+
+DROP TABLE IF EXISTS line;
 CREATE TABLE line (color char(10), station CHAR(4));
+
+DROP TABLE IF EXISTS station;
 CREATE TABLE station (name TEXT,abbr CHAR(4) PRIMARY KEY);
